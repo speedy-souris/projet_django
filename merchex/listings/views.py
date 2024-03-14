a115d9631bef0e6f37abee10a2ffae6dc054805d
Pascal MACCHI <pascal.macchi@gmail.com>
2024-03-14 04:25:11 +0100

    creation and configuration of 3 views in the linstings application

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def hello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>A propos</h1> <p>Nous adorons merch !</p>')
+
+def listings(request):
+    return HttpResponse("<h1>Listings</h1> <p>voici la liste d'annonce :</p>")
+
+def contact(request):
+    return HttpResponse('<h1>Contact</h1> <p>création de contact</p>')
