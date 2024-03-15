955c7675314ddecb00b0e76153de3e2549ed72d7
Pascal MACCHI <pascal.macchi@gmail.com>
2024-03-15 18:33:45 +0100

    modifying the article model and band model databases

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from listings.models import Band
+from listings.models import Article
 
+admin.site.register(Band)
+admin.site.register(Article)
 # Register your models here.
--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,13 +1,36 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
 class Band(models.Model):
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
     name = models.fields.CharField(max_length=100)
-    title = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2024)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+
 
 class Article(models.Model):
+    class Type(models.TextChoices):
+        RECORD ="RE"
+        CLOTHING = "CL"
+        POSTER = "PO"
+        MISCELLANEOUS = "MI"
+
     title = models.fields.CharField(max_length=100)
     date = models.fields.DateField()
+    types = models.fields.CharField(choices=Type.choices, max_length=5)
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=False)
 
 class Contact(models.Model):
     first_name = models.fields.CharField(max_length=30)
