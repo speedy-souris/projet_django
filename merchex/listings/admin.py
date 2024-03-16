233910e0dd975a04bcc6ca5286c2f2082fa71a98
Pascal MACCHI <pascal.macchi@gmail.com>
2024-03-16 08:03:45 +0100

    modification of the administration interface display for the band object

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from listings.models import Band
 from listings.models import Article
 
-admin.site.register(Band)
+
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year_formed', 'genre')
+
+admin.site.register(Band, BandAdmin)
 admin.site.register(Article)
 # Register your models here.
--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -18,6 +18,9 @@ class Band(models.Model):
     active = models.fields.BooleanField(default=True)
     official_homepage = models.fields.URLField(null=True, blank=True)
 
+    def __str__(self):
+        return f'{self.name}'
+
 
 class Article(models.Model):
     class Type(models.TextChoices):
